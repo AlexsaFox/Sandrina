184f2a2e13748af458db6c2c7886ee68687094b8
Aleksey <74228258+LeKSuS-04@users.noreply.github.com>
2021-05-22 10:57:52 +0300

    Updating top bar
    Added buttons to minimize and maximize window state; As well as project name on the top bar

---
+++ Sandrina/Sandrina/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sandrina {
+    public enum Mode {
+        Dark,
+        Light 
+	}
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+		#region Навигация по страницам
+		public void GotoInstructions(UIElement hide) {
+            hide.Visibility = Visibility.Hidden;
+            InstructionsPage.Visibility = Visibility.Visible;
+        }
+        public void GotoGame(UIElement hide) {
+            hide.Visibility = Visibility.Hidden;
+            GamePage.Visibility = Visibility.Visible;
+        }
+        public void ChangeTopBarColor(Color color) {
+            TopBar.Background = new SolidColorBrush(color);
+		}
+        public void ChangeTopBarButtonsMode(Mode mode) {
+            switch(mode) {
+                case Mode.Dark:
+                    Resources["BarButtonForeground"]  = new SolidColorBrush(Color.FromRgb(44, 62, 80));
+                    break;
+
+                case Mode.Light:
+                    Resources["BarButtonForeground"] = new SolidColorBrush(Color.FromRgb(221, 221, 221));
+                    break;
+			}
+		}
+		#endregion
+		#region Настройки окна
+		private void DragWindow(object sender, MouseButtonEventArgs e) {
+            if(e.ChangedButton == MouseButton.Left) {
+                this.DragMove();
+			}
+		}
+		private void CloseWindow(object sender, RoutedEventArgs e) {
+            Close();
+		}
+
+        private void HideWindow(object sender, RoutedEventArgs e) {
+            Window.GetWindow(this).WindowState = WindowState.Minimized;
+        }
+    
+        private void ResizeWindow(object sender, RoutedEventArgs e) {
+            if(Window.GetWindow(this).WindowState == WindowState.Maximized) {
+                Window.GetWindow(this).WindowState = WindowState.Normal;
+            } else {
+                Window.GetWindow(this).WindowState = WindowState.Maximized;
+            }
+        }
+        #endregion
+    }
+}
